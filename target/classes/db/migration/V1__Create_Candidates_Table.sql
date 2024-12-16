e126395cc33a9138124fdc9d9a978963255fd1f8
Nicolas Espinosa <nico.2011.pr@gmail.com>
2024-12-16 08:11:42 -0300

    Corrigiendo Dockerfile y otros ajustes

--- target/classes/db/migration/V1__Create_Candidates_Table.sql
+++ target/classes/db/migration/V1__Create_Candidates_Table.sql
@@ -1,12 +1,28 @@
+-- Crear la tabla candidates
 CREATE TABLE candidates (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,                 -- Usamos BIGSERIAL para la auto-incrementación
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     gender VARCHAR(10),
     expected_salary DECIMAL(10, 2),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Crear un trigger para actualizar el campo updated_at cuando se actualiza la fila
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;  -- Establece updated_at al momento actual
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Asignamos el trigger para actualizar updated_at cuando se haga un UPDATE en la tabla
+CREATE TRIGGER update_candidates_updated_at
+BEFORE UPDATE ON candidates
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
 
 -- Insertar datos de prueba
 INSERT INTO candidates (name, email, gender, expected_salary) VALUES
