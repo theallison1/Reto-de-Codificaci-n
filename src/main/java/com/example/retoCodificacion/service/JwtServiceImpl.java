1cd68747b88598e77c57219bc2d069290aedafa7
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2024-12-16 09:27:14 -0300

    Update JwtServiceImpl.java

--- src/main/java/com/example/retoCodificacion/service/JwtServiceImpl.java
+++ src/main/java/com/example/retoCodificacion/service/JwtServiceImpl.java
@@ -1,32 +1,72 @@
 package com.example.retoCodificacion.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
-@RequiredArgsConstructor
-@Slf4j
 public class JwtServiceImpl implements JwtService {
+    private final String secretKey ;
 
-    @Override
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        // Lógica para verificar si el token es válido
-        return true;  // Ejemplo, implementa la lógica adecuada
-    }
-
-    @Override
-    public String extractUsername(String token) {
-        // Lógica para extraer el nombre de usuario del token
-        return "username";  // Ejemplo, implementa la lógica adecuada
+    public JwtServiceImpl(@Value("${secret}") String secretKey) {
+        this.secretKey = secretKey;
     }
 
     @Override
     public String generateToken(Map<String, Object> extraClaims, String username) {
-        // Lógica para generar el token JWT
-        return "generated_token";  // Ejemplo, implementa la lógica adecuada
+        return Jwts.builder()
+                .addClaims(extraClaims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    @Override
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Key getSignInKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token)  {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
     }
 }
